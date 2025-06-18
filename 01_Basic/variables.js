098d64541a8c623618ce2a897273b8332e31d740
umarnaxir <erumarnazir@gmail.com>
2025-06-18 15:13:57 +0530

    added

--- 01_Basic/variables.js
+++ 01_Basic/variables.js
@@ -1,2 +1,10 @@
-let userName = 'umar'; // umar is value and let username is a variables
+let userName = 'Amir'; // umar is value and let username is a variables, 
+// this is a big advantages of variables, we have to change the variable name once and it changes automatically
 console.log(userName);
+console.log(userName);
+console.log(userName);
+
+let firstName = 'Umar';
+let lastName = 'Nazir';
+let age = 23;
+console.log(firstName, lastName, age);
