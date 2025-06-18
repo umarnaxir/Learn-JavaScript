624f5296f280571439bdb967a57640121f047846
umarnaxir <erumarnazir@gmail.com>
2025-06-18 16:04:27 +0530

    added

--- 01_Basic/variables.js
+++ 01_Basic/variables.js
@@ -6,5 +6,18 @@ console.log(userName);
 
 let firstName = 'Umar';
 let lastName = 'Nazir';
-let age = 23;
-console.log(firstName, lastName, age);
+let personAge = 23;
+console.log(firstName, lastName, personAge);
+
+//var, let, const
+let age = 30;
+age = 31;
+console.log(`This is let variable`, age);  //you can change the value of let variable
+
+const birthYear = 2000;
+// birthYear = 2001;          //but you can not change the value of const
+console.log(`This is const variable`, birthYear);
+
+var a = 10;
+var a = 20; //you can change the value of var variable, but its a old version
+console.log(`This is var variable`, a);
