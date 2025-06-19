0ebc6b7e86771f2ef4fa5ccf0533e0e7e43a1495
umarnaxir <erumarnazir@gmail.com>
2025-06-20 01:52:49 +0530

    added

---
+++ 01_Basic/09_LogicalOperators.js
@@ -0,0 +1,5 @@
+const hasDriveLicense = true; //A
+const hasGoodVision = false; //B
+console.log(hasDriveLicense && hasGoodVision); //And Operator
+console.log(hasDriveLicense || hasGoodVision); //Or Operator
+console.log(!hasDriveLicense); //Not Operator
