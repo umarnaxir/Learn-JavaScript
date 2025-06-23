d1675173de7c8000934fb401d8cc8f08b472ae85
umarnaxir <erumarnazir@gmail.com>
2025-06-23 08:37:26 +0530

    added

---
+++ 01_Basic/11_Ternary.js
@@ -0,0 +1,9 @@
+const age = 10;
+age >= 18 ? console.log("adult") : console.log("Not adult");
+
+//example 2
+const drink = age >= 18 ? ('wine') : ('water');
+console.log(drink);
+
+//example 3
+console.log(`I like to drink ${age>=18 ? 'juice' : 'water'}`);
