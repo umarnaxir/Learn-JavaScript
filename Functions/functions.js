cd90e4219e4191e58149eba77e2c0ef1b52fbb23
umarnaxir <erumarnazir@gmail.com>
2025-06-24 13:23:11 +0530

    added

--- Functions/functions.js
+++ Functions/functions.js
@@ -1,3 +1,4 @@
+//Exapmle 1
 function checkAge(age){
   if(age>=50){
     return "Senior";
@@ -10,3 +11,26 @@ function checkAge(age){
   }
 }
 console.log(checkAge(55));
+
+//Example 2
+function getGrade(marks){
+  switch(true){
+    case marks>=90:
+      return "A";
+      break;
+      case marks>=75:
+      return" B";
+      break;
+      case marks>=50:
+      return "C";
+      default:
+      return" F";
+  }
+}
+console.log(getGrade(99));
+
+//Example 3
+function maxOfThree(a,b,c){
+  return Math.max(a,b,c)
+}
+console.log(maxOfThree(10,20,30))
