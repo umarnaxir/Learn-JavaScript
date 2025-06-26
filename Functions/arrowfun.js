da7a305d8bb5231d334eb9af3220c0c3b3a31716
umarnaxir <erumarnazir@gmail.com>
2025-06-26 11:59:42 +0530

    added

--- Functions/arrowfun.js
+++ Functions/arrowfun.js
@@ -8,6 +8,7 @@ console.log(calAge1(1999));
 const calAge2 = birthYear => 2025 - birthYear;
 console.log(calAge2(2000));
 
+//Exaple of arrow function
 const yearsUntilRetirement = (birthYear, firstName)=>{
     const age = 2025 - birthYear;
     const retirement = 65 - age;
@@ -15,3 +16,15 @@ const yearsUntilRetirement = (birthYear, firstName)=>{
     return `${firstName} retires in ${retirement} Years`;
 }
 console.log(yearsUntilRetirement(2001, "umar"));
+
+//function calling in function
+function cutFruitPieces(fruit){
+    return fruit * 4;
+}
+function fruitProcessor(apples, oranges){
+const applePieces = cutFruitPieces(apples);
+const oranePieces = cutFruitPieces(oranges);
+const juice = `Juice with ${applePieces} pieces of apple and ${oranePieces} pieces of orange.`;
+return juice;
+}
+console.log(fruitProcessor(3,4));
