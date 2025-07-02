18a694f415335206b605f4632683c79c1592670d
umarnaxir <erumarnazir@gmail.com>
2025-07-02 07:36:35 +0530

    added

--- Objects/methods.js
+++ Objects/methods.js
@@ -54,3 +54,44 @@ const user3 = {
     }
 }
 console.log(user3.getFullName())
+
+//Costructors
+function Person(first, last){
+    this.firstName = first,
+    this.lastName = last
+}
+const person1 = new Person("Amir", "Nazir");
+const person2 = new Person("Umar", "Nazir");
+console.log(person1);
+console.log(person2);
+person1.age = 50;
+console.log(person1);
+
+//Prototype
+function Person(){
+    this.name = "Elon Musk"
+}
+Person.prototype.age = 30;
+const person3 = new Person();
+Person.prototype = {age:52}
+const person4 = new Person()
+console.log(person3.age);
+console.log(person4.age);
+
+//Deconstruction
+const person5 = {
+    firstName: "Khalid",
+    lastName: "Jan",
+    age: 24
+}
+let { firstName, lastName, age =18} = person5; //age = 18 is a default value
+console.log(person5)
+
+//Object Literal Syntax Extensions
+let fName = "Faizan";
+let lName = "Khan";
+const person6 = {
+    fName,
+    lName,
+}
+console.log(person6)
