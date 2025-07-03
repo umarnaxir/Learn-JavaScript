573b1be73ec1d305adab77c0f4a6a90a75172bf3
umarnaxir <erumarnazir@gmail.com>
2025-07-03 22:56:04 +0530

    added

--- index.js
+++ index.js
@@ -1,38 +1,10 @@
-// let sum = 0;
-// const numbers = [25, 15, 30, 10];
-// for (let i = 0; i < numbers.length; i++) {
-//   sum = sum + numbers[i];
-  
-//   console.log(`Sum of all Numbers`, sum);
-// }
-
-// Print Max number
-// const numbers = [1200, 1850, 900, 1600, 2000, 1750, 2100];
-// let max = numbers[0];
-// for(let i = 0; i < numbers.length; i++){
-//     if(numbers[i] > max){
-//         max = numbers[i];
-//     }
-// }
-// console.log("Maximum number:", max);
-
-// Print Min number
-// const numbers = [1200, 1850, 900, 1600, 2000, 1750, 2100];
-// let min = numbers[0];
-// for(let i = 0; i < numbers.length; i++){
-//     if(numbers[i] < min){
-//         min = numbers[i];
-//     }
-// }
-// console.log("Minimum number:", min);
-
-// Print first and last number
-// const numbers = [1200, 1850, 900, 1600, 2000, 1750, 2100];
-// for(let i = 0; i < numbers.length; i++){
-//     if(i === 0){
-//         console.log("First number:", numbers[i]);
-//     }
-//     if(i === numbers.length - 1){
-//         console.log("Last number:", numbers[i]);
-//     }
-// }
+const a = {name:"umar", age:24}
+const b = {email:"umar@123"}
+const ab = Object.assign({},a,b)
+console.log(ab);
+for(let umar in ab){
+    console.log(umar + " " + ab[umar])
+}
+console.log(Object.entries(ab))
+console.log(a.hasOwnProperty("name"))
+console.log(ab.hasOwnProperty("amir"))
