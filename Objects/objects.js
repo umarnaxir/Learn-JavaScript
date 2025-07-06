8570bc1f3aa46168a45923ec24a52927ecfff2b4
umarnaxir <erumarnazir@gmail.com>
2025-07-06 18:21:30 +0530

    added

--- Objects/objects.js
+++ Objects/objects.js
@@ -1,100 +1,82 @@
-// //array
-// const umarArray = [
-//   'umar',
-//   'nazir',
-//   2025 - 2001,
-//   ['Owais', 'Khalid', 'OTL']
-// ];
+//array
+const umarArray = [
+  'umar',
+  'nazir',
+  2025 - 2001,
+  ['Owais', 'Khalid', 'OTL']
+];
 
-// //object
-// const umar = {
-//   firstName: 'Umar',
-//   lastName: 'Nazir',
-//   age: 2025 - 2001,
-//   job: 'Intern',
-//   friends:['Owais', 'Khalid', 'OTL']
-// }
-// console.log(umar);
-// console.log(umar.lastName);
-// console.log(umar.age);
+//object
+const umar = {
+  firstName: 'Umar',
+  lastName: 'Nazir',
+  age: 2025 - 2001,
+  job: 'Intern',
+  friends:['Owais', 'Khalid', 'OTL']
+}
+console.log(umar);
+console.log(umar.lastName);
+console.log(umar.age);
 
-// //Object Methods
+//Object Methods
 
-// //Asign
-// const person = {
-//   firstName: "Amir",
-//   lastName: "Nazir"
-// }
-// const xyz = {
-//   firstName: "Faizan",
-//   age: 20,
-//   Email: "faizan@123"
-// }
-// console.log(person);
-// Object.assign(person, xyz);
-// console.log(person);
+//Asign
+const person = {
+  firstName: "Amir",
+  lastName: "Nazir"
+}
+const xyz = {
+  firstName: "Faizan",
+  age: 20,
+  Email: "faizan@123"
+}
+console.log(person);
+Object.assign(person, xyz);
+console.log(person);
 
-// //Create
-// const person2 = {
-//   firstName: "sameer",
-//   age: 23
-// }
-// const newObj = Object.create(person2);
-// console.log(newObj.firstName);
+//Create
+const person2 = {
+  firstName: "sameer",
+  age: 23
+}
+const newObj = Object.create(person2);
+console.log(newObj.firstName);
 
-// //Keys
-// const person3 = {
-//   firstName: "Amir",
-//   lastName: "Nazir",
-//   age:22,
-//   Email: "amir@123"
-// }
-// const myKeys = Object.keys(person3)
-// console.log(myKeys);
-// console.log(Object.keys(person3));
+//Keys
+const person3 = {
+  firstName: "Amir",
+  lastName: "Nazir",
+  age:22,
+  Email: "amir@123"
+}
+const myKeys = Object.keys(person3)
+console.log(myKeys);
+console.log(Object.keys(person3));
 
-// //values
-// console.log(Object.values(person3));
+//values
+console.log(Object.values(person3));
 
-// //entries
-// console.log(Object.entries(person3));
+//entries
+console.log(Object.entries(person3));
 
-// //fromEntries - back to obj
-// const newObj2 = Object.fromEntries(Object.entries(person3));
-// console.log(newObj2);
+//fromEntries - back to obj
+const newObj2 = Object.fromEntries(Object.entries(person3));
+console.log(newObj2);
 
-// //Example 2
-// const entries = [
-//   ["firstName", "Amir"],
-//   ["lastName", "Nazir"],
-//   ["age", 22],
-//   ["email", "amir@123"]
-// ];
+//Example 2
+const entries = [
+  ["firstName", "Amir"],
+  ["lastName", "Nazir"],
+  ["age", 22],
+  ["email", "amir@123"]
+];
 
-// const newEntry = Object.fromEntries(entries);
+const newEntry = Object.fromEntries(entries);
 
-// console.log(newEntry);
+console.log(newEntry);
 
-// //GroupBy - Looping (Grouping of data)
-// const flipKart = [
-//   { productName: 'Redmi Note 7', category: 'mobile', price: 16000 },
-//   { productName: 'Phillips 001', category: 'TV', price: 22000 },
-//   { productName: 'MI Smart TV', category: 'LED', price: 35000 },
-//   { productName: 'Samsung Galaxy', category: 'mobile', price: 25000 },
-//   { productName: 'Elephent 005', category: 'Toy', price: 1499 },
-//   { productName: 'Nokia n1054', category: 'LED', price: 50000 },
-//   { productName: 'Sony', category: 'TV', price: 70000 },
-// ];
-
-// const result = Object.groupBy(flipKart, (item) => {
-//   if (item.price >= 25000) {
-//     return 'HighPriceProducts';
-//   } else {
-//     return 'LowPriceProducts';
-//   }
-// });
-
-// console.log(result);
+//GroupBy - Looping (Grouping of data)
+//General Methods
 
 const flipKart = [
   { productName: 'Redmi Note 7', category: 'mobile', price: 16000 },
@@ -106,6 +88,14 @@ const flipKart = [
   { productName: 'Sony', category: 'TV', price: 70000 },
 ];
 
+const result = Object.groupBy(flipKart, (item) => {
+  if (item.price >= 25000) {
+    return 'HighPriceProducts';
+  } else {
+    return 'LowPriceProducts';
+  }
+});
+
 // Grouping by renamed category labels
 const catItem = Object.groupBy(flipKart, (item) => {
   if (item.category === "LED") return "LedProducts";
@@ -116,3 +106,16 @@ const catItem = Object.groupBy(flipKart, (item) => {
 });
 
 console.log(catItem);
+console.log(result);
+//General Methods End
+
+//Prevent methods - Secure 
+const person4 = {
+  firstName: "umar",
+  lastName: "nazir",
+  age: 24,
+  email: "umar@123"
+}
+Object.preventExtensions(person4);
+person4.mob = 234545678;
+console.log(person4);
